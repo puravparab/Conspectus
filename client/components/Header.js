92cf396bb2ee10b3c9ca6ba40040036bb7f0f479
Purav <puravparabdev@gmail.com>
2023-05-20 23:59:40 +0400

    header update

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 import styles from '../styles/header.module.css'
 
@@ -30,7 +31,6 @@ const Header = () => {
 
 	const getTheme = () => {
 		const theme = localStorage.getItem('theme')
-		console.log(theme)
 		if (theme == 'dark'){return 'dark'} 
 		else{ return 'light'}
 	}
@@ -39,8 +39,21 @@ const Header = () => {
 		<div className={styles.headerContainer}>
 			<div className={styles.headerMain}>
 				<div className={styles.headerTitle}>
-					<h1>Conspectus</h1>
+					<Link href='/'><h1>Conspectus</h1></Link>
 					<p>Welcome, Purav!</p>
+				</div>
+
+				{/* Navbar */}
+				<div className={styles.navbar}>
+					<div className={styles.navItem}>
+						<Link href="/contacts"><h2 className={router.pathname == '/contacts'? styles.navActive: ""}>Contacts</h2></Link>
+					</div>
+					<div className={styles.navItem}>
+						<Link href="/orgs"><h2 className={router.pathname == '/orgs'? styles.navActive: ""}>Orgs</h2></Link>
+					</div>
+					<div className={styles.navItem}>
+						<Link href="/notes"><h2 className={router.pathname == '/notes'? styles.navActive: ""}>Notes</h2></Link>
+					</div>
 				</div>
 
 				<div className={styles.headerRight}>
