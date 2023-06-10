98c70a0b578358c0f0bc8a27886f9acffcea86be
Purav <puravparabdev@gmail.com>
2023-06-11 01:20:02 +0400

    added contacts list to map

--- client/components/Contacts/Map/ContactMap.js
+++ client/components/Contacts/Map/ContactMap.js
@@ -4,6 +4,8 @@ import axios from 'axios';
 import getConfig from 'next/config';
 import 'leaflet/dist/leaflet.css'
 import { MapContainer, TileLayer, CircleMarker, Tooltip} from 'react-leaflet'
+
+import { AddContactModal } from '../ContactForm.js'
 import styles from '../../../styles/contacts.module.css'
 
 const { publicRuntimeConfig } = getConfig();
@@ -14,6 +16,7 @@ const ContactMap = ( props ) => {
 	// // // //
 	
 	const [contactData, setContactData] = useState(null)
+	const [contactLength, setContactLength] = useState(0)
 	const [renderData, setRenderData] = useState(null)
 	const [showCurrentCity, setShowCurrentCity] = useState(true)
 
@@ -40,21 +43,40 @@ const ContactMap = ( props ) => {
 		let contact_by_city = {}
 		let city = ""
 		for (let i=0; i < data.length; i++){
-			if (showCurrentCity){city = data[i].current_location_city}
-			else{city = data[i].original_location_city}
+			if (showCurrentCity){
+				city = data[i].current_location_city
+				setSelectedCity("San Francisco")
+			}
+			else{
+				city = data[i].original_location_city
+				setSelectedCity("Davis")
+			}
 			
 			if (city === null){continue}
 			if (!contact_by_city[city]){contact_by_city[city] = []}
 			contact_by_city[city].push(data[i])
 		}
 		setRenderData(contact_by_city)
+		setContactLength(data.length)
+	}
+
+	useEffect(() => {
+		if (contactData){
+			processData(contactData)
+		}
+	}, [showCurrentCity])
+
+	const handleMapType = (type) => {
+		if (type === "current"){setShowCurrentCity(true)}
+		else if (type === "initial"){setShowCurrentCity(false)}
 	}
 
 	// // // //
 	// CITIES DATA
 	// // // //
 
-	const [citiesData, setCitiesData] = useState(null);
+	const [citiesData, setCitiesData] = useState(null)
+	const [selectedCity, setSelectedCity] = useState(null)
 
 	useEffect(() =>{
 		axios.get('/cities.json')
@@ -66,6 +88,11 @@ const ContactMap = ( props ) => {
 			})
 	}, [])
 
+	const handleSelectedCity = (city) => {
+		setSelectedCity(city)
+	}
+
+	
 	// // // //
 	// MODALS
 	// // // //
@@ -79,11 +106,15 @@ const ContactMap = ( props ) => {
 
 	return (
 		<div className={styles.contactsMapContainer}>
+			{showModal && <AddContactModal handleAddContact={handleAddContact}/>}
+
 			<div className={styles.contactDashHeader}>
 				<div className={styles.contactDashTitle}>
 					<h3>Contact Map</h3>
-					{/* <p>{dataLength.total} total contacts</p> */}
-					<p>0 total contacts</p>
+					<p>{contactLength} total contacts</p>
+					{renderData &&
+						<p>{Object.keys(renderData).length} total cities</p>
+					}
 				</div>
 				<div className={styles.contactDashOther}>
 					<button onClick={handleAddContact}><span>+ Add Contact</span></button>
@@ -111,6 +142,7 @@ const ContactMap = ( props ) => {
 										fillOpacity={0.7}
 										stroke={false}
 										pathOptions={{ color: 'var(--dark-primary-heading)' }}
+										eventHandlers={{ click: () => {handleSelectedCity(city)}}}
 									>
 										<Tooltip direction="right" offset={[-8, -2]} opacity={1}>
 											<span>{city}: {contacts.length} contacts</span>
@@ -123,7 +155,33 @@ const ContactMap = ( props ) => {
 				</MapContainer>
 
 				<div className={styles.contactsMapMainRight}>
-					
+					<div className={styles.contactsMapMainRightHeader}>
+						{selectedCity &&
+							<>
+								<h4>{selectedCity}</h4>
+								<p>{renderData[selectedCity].length} total contacts</p>
+							</>
+						}
+					</div>
+					<div className={styles.contactsMapMainRightOptions}>
+						<div onClick={() => {handleMapType("current")}}><span>Current</span></div>
+						<div onClick={() => {handleMapType("initial")}}><span>Initial</span></div>
+					</div>
+					{selectedCity &&
+						<div className={styles.contactsMapMainRightContent}>
+							{renderData[selectedCity].map((contact, id) => {
+								return (
+									<div className={styles.contactsMapMainItem} key={id}>
+										{contact.image? 
+											<img width="30" height="30" src={contact.image} alt={`${contact.name} image`} />
+											:<img width="30" height="30" src="https://img.icons8.com/doodle/96/user-male-circle.png" alt="user-male-circle"/>
+										}
+										<p>{contact.name}</p>
+									</div>
+								)
+							})}
+						</div>
+					}
 				</div>
 			</div>
 		</div>
